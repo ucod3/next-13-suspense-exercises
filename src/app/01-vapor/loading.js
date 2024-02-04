df695caac955cf0218e35e80ebf05db43884173c
Usman Butt <usman.butt@outlook.com>
2024-02-04 08:30:26 -0500

    Refactor VaporExercise to VaporExerciseLoading

--- src/app/01-vapor/loading.js
+++ src/app/01-vapor/loading.js
@@ -4,19 +4,19 @@ import LibraryGameCardSkeleton from '@/components/LibraryGameCardSkeleton';
 
 export const dynamic = 'force-dynamic';
 
-async function VaporExercise() {
+async function VaporExerciseLoading() {
   return (
     <section className='max-width-wrapper'>
       <header className='library-header'>
         <h1>My games</h1>
       </header>
       <div className='game-grid'>
-        {range(0, 12).map((index) => (
-          <LibraryGameCardSkeleton key={index} />
+        {range(0, 12).map((num) => (
+          <LibraryGameCardSkeleton key={num} />
         ))}
       </div>
     </section>
   );
 }
 
-export default VaporExercise;
+export default VaporExerciseLoading;
