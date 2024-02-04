cc267fbc8228e45ba6c49c467d69f7b41c128562
Usman Butt <usman.butt@outlook.com>
2024-02-04 12:45:51 -0500

    Refactor getNavLinks to use named function expression

--- src/helpers/web-base-helpers.js
+++ src/helpers/web-base-helpers.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const getNavLinks = React.cache(async () => {
+export const getNavLinks = React.cache(async function getNavLinks() {
   console.info('Requesting navigation links from CMS');
   await delay(1800);
 
