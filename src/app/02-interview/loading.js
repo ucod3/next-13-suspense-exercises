95050a43844cdb52d877aebae053d07e74978ab9
Usman Butt <usman.butt@outlook.com>
2024-02-04 08:56:47 -0500

    Add loading component for interview exercise

---
+++ src/app/02-interview/loading.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import ArticleWrapper from './ArticleWrapper';
+import Spinner from '@/components/Spinner';
+
+async function InterviewExercise() {
+  return (
+    <ArticleWrapper>
+      <Spinner />
+    </ArticleWrapper>
+  );
+}
+
+export default InterviewExercise;
