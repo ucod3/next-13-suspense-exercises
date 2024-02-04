7e0ee2e7efbc676843e2f2810d751448e361be74
Usman Butt <usman.butt@outlook.com>
2024-02-04 08:44:27 -0500

    Refactor VaporExercise function to use ArticleWrapper component

--- src/app/01-vapor/page.js
+++ src/app/01-vapor/page.js
@@ -3,31 +3,17 @@ import React from 'react';
 import { getGamesInLibrary } from '@/helpers/vapor-helpers';
 import LibraryGameCard from '@/components/LibraryGameCard';
 
-export const dynamic = 'force-dynamic';
+import ArticleWrapper from './ArticleWrapper';
 
 async function VaporExercise() {
   const games = await getGamesInLibrary();
 
   return (
-    <section className="max-width-wrapper">
-      <header className="library-header">
-        <h1>My games</h1>
-        <p>
-          <span className="num-of-games">
-            {games.length}
-          </span>{' '}
-          games in library
-        </p>
-      </header>
-      <div className="game-grid">
-        {games.map((game) => (
-          <LibraryGameCard
-            key={game.slug}
-            game={game}
-          />
-        ))}
-      </div>
-    </section>
+    <ArticleWrapper numOfGames={games.length}>
+      {games.map((game) => (
+        <LibraryGameCard key={game.id} game={game} />
+      ))}
+    </ArticleWrapper>
   );
 }
 
