a26a081e06943f8c2e3b0f73b2c47f08654e6f99
Usman Butt <usman.butt@outlook.com>
2024-02-04 08:44:23 -0500

    Refactor loading.js to use ArticleWrapper component

--- src/app/01-vapor/loading.js
+++ src/app/01-vapor/loading.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import { range } from '@/utils';
 import LibraryGameCardSkeleton from '@/components/LibraryGameCardSkeleton';
-
-export const dynamic = 'force-dynamic';
+import ArticleWrapper from './ArticleWrapper';
 
 async function VaporExerciseLoading() {
   return (
-    <section className='max-width-wrapper'>
-      <header className='library-header'>
-        <h1>My games</h1>
-      </header>
-      <div className='game-grid'>
-        {range(0, 12).map((num) => (
-          <LibraryGameCardSkeleton key={num} />
-        ))}
-      </div>
-    </section>
+    <ArticleWrapper>
+      {range(12).map((num) => (
+        <LibraryGameCardSkeleton key={num} />
+      ))}
+    </ArticleWrapper>
   );
 }
 
